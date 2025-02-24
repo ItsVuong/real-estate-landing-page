b70c49a649089b07b387a7165f6bfb98bf34b0a2
ItsVuong <itsvuong0806@gmail.com>
2025-02-25 03:21:38 +0700

    Fix typos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -59,7 +59,7 @@ export const Footer = () => {
               </svg>
             </a>
             <a
-              href="https://www.facebook.com/profile.php?id=61573782980754"
+              href="https://zalo.me/0367083529?src=qr"
               target="_blank"
               rel="noopener noreferrer"
               className="text-primary"
